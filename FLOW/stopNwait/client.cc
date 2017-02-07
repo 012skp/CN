9662450d0ee524e06d1ab92fe88a9a7dc94654ff
Shailesh Kumar <012skp@gmail.com>
2017-02-07 19:05:08 +0530

    some files deleted..

--- FLOW/stopNwait/client.cc
+++ FLOW/stopNwait/client.cc
@@ -11,31 +11,37 @@ struct frame{
 int main(int argc, char* argv[]){
 if(argc<3){printf("usage %s <ip> <port>\n",argv[0]);exit(1);}
 int sockfd = connect_server(argv[1],atoi(argv[2]));
-int n = 1;
+int n = 0;
 int s = 0;
 
-struct timeval tv;
-tv.tv_sec = 3;
-tv.tv_usec = 0;
-fd_set fds;
+
 
 while(1){
 	char msg[100];
+	bzero(msg,sizeof(msg));
 	sprintf(msg,"this is frame no: %d", n);
 	frame f;
 	strcpy(f.data,msg);
 	f.seq = s;
+	printf("sending frame--> %s seq = %d\n",f.data,f.seq);
 	write(sockfd,&f,sizeof(f));
 	char ack[100];
 
 	//preparing for timeout..
+	struct timeval tv;
+	tv.tv_sec = 15;
+	tv.tv_usec = 0;
+	fd_set fds;
 	FD_ZERO(&fds);
 	FD_SET(sockfd,&fds);
 	if(select(sockfd+1,&fds,NULL,NULL,&tv)){
+		bzero(ack,sizeof(ack));
 		read(sockfd,ack,sizeof(ack));
 		n++;
 		s = s+1;
 		s = s%2;
+		printf("ACK received...%d\n",atoi(ack));
+		//printf("ACD data = %s\n",ack+10);
 	}
 	else{
 		printf("timeout resending..\n");
--- FLOW/stopNwait/server.cc
+++ FLOW/stopNwait/server.cc
@@ -12,24 +12,34 @@ int main(int argc, char* argv[]){
 int sockfd = host_server(atoi(argv[1]));
 int newsockfd = accept_connection(sockfd);
 int s = 0;
+int nn;
 while(1){
 	frame f;
-	read(newsockfd,(char*)&f,sizeof(f));	
-	printf("frame received...%s\n",f.data);
+	printf("waiting for seq = %d\n",s);
+	if(read(newsockfd,(char*)&f,sizeof(f))<=0){ printf("client disconnected..\n"); exit(1);}	
+	printf("frame receiving..%s\n",f.data);
+	printf("want to drop the frame(1/0):");
+	scanf("%d",&nn);
+	if(nn) continue;
+	printf("Frame successfully received: %s seq = %d\n",f.data,f.seq);
 	if(f.seq != s){
 		printf("Wrong frame received..\n"); 
 		char ack[100];
 		sprintf(ack,"%d",s);
+		//strcpy(ack+10,"Piggybacking....\0");
+		printf("sending ACK %d\n",atoi(ack));
 		write(newsockfd,ack,sizeof(ack));
+		continue;
 	}
-	printf("want to send ack(1/0):");
-	int nn;
-	scanf("%d",&nn);
+	printf("sending ACK...\n");
 	char ack[100];
 	s = s+1;
 	s = s%2;
 	sprintf(ack,"%d",s);
-	write(newsockfd,ack,sizeof(ack));
+	//strcpy(ack+10,"Piggybacking....\0");
+	printf("want to drop the ack(1/0):");
+	scanf("%d",&nn);
+	if(!nn) write(newsockfd,ack,sizeof(ack));
 
 }
 
