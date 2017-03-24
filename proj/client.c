b91846027d4281893175969fe0df04f1eb1361f1
012skp <012skp@gmail.com>
2017-03-24 18:45:41 +0530

    sdlkf

--- proj/client.c
+++ proj/client.c
@@ -26,7 +26,7 @@ struct addr{
 };
 
 struct msg{
-	int fun; //1 getlist 2.naming 3.killing closed clients 4.Broadcasting 5.Newconnection 
+	int fun; //1 getlist 2.naming 3.killing closed clients 4.Broadcasting 5.Newconnection
 	char message[256];
 	struct addr dstn;
 	struct addr src;
@@ -41,19 +41,19 @@ int main(int argc,char **argv)
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
 	serv_addr.sin_port = htons(atoi(argv[2]));
-	
 
-	sockfd = socket(AF_INET,SOCK_STREAM,0);	
+
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
 	if(sockfd == -1)
-	{ 
+	{
 		printf("Error creating socket...");
-		return 1; 
+		return 1;
 	}
 
 	if( connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0)
 	{
-		printf("\nError connecting to server...."); 
-		return 1; 
+		printf("\nError connecting to server....");
+		return 1;
 	}
 
 	char str[25];
@@ -68,7 +68,7 @@ int main(int argc,char **argv)
 	write(sockfd,(char *)data,sizeof(struct msg));
 	struct msg *data1=(struct msg *)malloc(sizeof(struct msg));
 
-	
+
 	/*
 	struct timeval tv;
 	tv.tv_sec = 5;
@@ -91,10 +91,10 @@ int main(int argc,char **argv)
 	HELP
 	int pid = fork();
 	int n=0;
-	
-	
 
-	
+
+
+
 	while(1){
 
 		if(pid == 0){
@@ -113,25 +113,25 @@ int main(int argc,char **argv)
 			}
 
 
-			else if(sscanf(str,"send_%d",&data->dstn.id) > 0) 
+			else if(sscanf(str,"send_%d",&data->dstn.id) > 0)
 			while(1){printf(":");
 				data->fun = 0;
 				fflush(stdin); scanstr(data->message);
 				if(!strcmp(data->message,"disconnect")) break;
-				if( write(sockfd,(char *)data,sizeof(struct msg)) < 0 ) 
+				if( write(sockfd,(char *)data,sizeof(struct msg)) < 0 )
 				printf("\nSorry Message could not be sent..");
 			}
 			else HELP
 		}
 
-		
+
 
 		if(pid > 0){
 			//char str[sizeof(struct msg)];
-			
+
 			//bzero(str,sizeof(str));
 			bzero((char *)data1,sizeof(struct msg));
-			if(read(sockfd,(char*)data1,sizeof(struct msg)) > 0 ) 
+			if(read(sockfd,(char*)data1,sizeof(struct msg)) > 0 )
 			{
 				//data1 = (struct msg *)str;
 				printf("\n(\x1b[32m%d\x1b[0m)\x1b[32m%s\x1b[0m: \x1b[32m%s\x1b[0m\n",data1->src.id,data1->src.name,data1->message);}
@@ -147,13 +147,6 @@ int main(int argc,char **argv)
 
 
 
-	
+
 	printf("\n....\n"); return 0;
 }
-
-
-
-
-
-
-
